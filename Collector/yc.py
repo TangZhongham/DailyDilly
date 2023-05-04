e9e946425407f41bcb5d4cf36a5b3b6fd37acb1c
tzh <Tangzhongham@gmail.com>
2023-05-04 16:48:30 +0800

    finish raw yc hackernews scraping

--- Collector/yc.py
+++ Collector/yc.py
@@ -3,7 +3,10 @@ import re
 
 url = "https://news.ycombinator.com/"
 
-today_yc = {}
+ycs = {}
+today_yc = []
+a = []
+b = []
 
 
 def get_yc(yc):
@@ -13,29 +16,42 @@ def get_yc(yc):
     # url
     for i in p.find_all("span", "titleline"):
         # print(i)
-        today_yc["title"] = i.get_text()
-        today_yc["url"] = i.find("a").get("href")
+        title_dic = {"title": i.get_text(), "url": i.find("a").get("href")}
+        a.append(title_dic)
 
     # 作者，points 数，时间。（可以获得当天内的top10帖子
     for x in p.find_all("td", "subtext"):
-        # print(x)
+        score_dic = {}
         # 存在没有score 的条目
         _score = x.find("span", "score").get_text() if x.find("span", "score") else ""
         if re.findall("[0-9]+", _score):
             score = re.findall("[0-9]+", _score)[0]
-            print(score)
+            score_dic["score"] = score
         # '2023-05-03T10:32:10'
         timestamp = x.find("span", "age").get("title")
-        today_yc["timestamp"] = timestamp
+        score_dic["timestamp"] = timestamp
         # time from now
         time = x.find("span", "age").get_text()
-        today_yc["time"] = time
+        score_dic["time"] = time
         writer = x.find("a", "hnuser").get_text() if x.find("a", "hnuser") else ""
+        score_dic["writer"] = writer
         _comments = x.find_all("a")[-1].get_text()
         if re.findall("[0-9]+", _comments):
             comments = re.findall("[0-9]+", _comments)[0]
-            print(comments)
+            score_dic["comments"] = comments
+        b.append(score_dic)
 
 
 if __name__ == '__main__':
     get_yc(url)
+    final = []
+    for x, y in zip(a,b):
+        z = x | y
+        final.append(z)
+    # [{'title': 'Prime Video ', 'url': 'https://www.primevideotech.com', 'score': '444', 'timestamp':
+    # '2023-05-04T06:04:56', 'time': '2 hours ago', 'writer': 'debdut', 'comments': '204'} ]
+    print(a)
+    print(len(a))
+    print(b)
+    print(len(b))
+    print(final)
